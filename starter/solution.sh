f5bf4a080d10d0c1908c2f317acfd06012f34f89
pavithraselvam17 <pavithraselvam53@gmail.com>
2026-08-31 19:58:02 +0530

    UNIX PROGRAM8 1U25IT068

--- starter/solution.sh
+++ starter/solution.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Required files and relative directories create செய்ய
-touch file1.txt file2.txt
-mkdir -p documents
-mkdir -p directory
-
-# 1. Copy a single file
+# Copy a single file
 cp file1.txt file2.txt
 
-# 2. Copy a file to a different directory
+# Copy a file to a different directory
 cp file1.txt documents/
 
-# 3. Copy multiple files to a directory
+# Copy multiple files to a directory
 cp file1.txt file2.txt documents/
 
-# 4. Copy a directory recursively
+# Copy a directory recursively
 cp -r directory documents/
