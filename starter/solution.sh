d84b2f84daaf3e573ef1c8015e43dc16bce8b10a
pavithraselvam17 <pavithraselvam53@gmail.com>
2026-08-31 19:49:07 +0530

    PROGRAM 8 1U25IT068

--- starter/solution.sh
+++ starter/solution.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Copy a single file
+cp file1.txt file2.txt
+
+# Copy a file to a different directory
+cp file1.txt /home/user/documents/
+
+# Copy multiple files to a directory
+cp file1.txt file2.txt /home/user/documents/
+
+# Copy a directory recursively
+cp -r directory /home/user/documents/
